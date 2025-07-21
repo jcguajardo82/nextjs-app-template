3971a185ede88d3b20cef6e5f18bfc65b3bc866b
jcguajardo82 <82547600+jcguajardo82@users.noreply.github.com>
2025-07-21 13:33:47 -0600

    Add .next/static/chunks/src_app_page_tsx_18625679._.js 1753126427097

---
+++ .next/static/chunks/src_app_page_tsx_18625679._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_73ab95fd._.js",
+  "static/chunks/node_modules_next_15251bc2._.js"
+],
+    source: "dynamic"
+});
